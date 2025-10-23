e97532171fb4a80494d5aa7230de1a163547474b
ibeO-GH <ibe.okorafor@gmail.com>
2025-10-23 22:45:29 +0100

    tailwind fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
 
 const queryClient = new QueryClient();
 
